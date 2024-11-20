60ab83f2d84f4f6a77fec5acd236dfc28c090b38
Kaito Ohira <221140057p@ed.fuk.kindai.ac.jp>
2024-11-20 07:51:10 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get "order/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   get 'top/main'
   post 'top/login'
   get 'top/logout'
